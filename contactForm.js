fb51f65977be04249cedd9fcee984747a165efef
bharatkumar00797 <154852574+bharatkumar00797@users.noreply.github.com>
2024-02-08 14:49:31 -0500

    update1

--- contactForm.js
+++ contactForm.js
@@ -1,45 +1,70 @@
-// Make sure to run npm install @formspree/react
-// For more help visit https://formspr.ee/react-help
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
+
 function ContactForm() {
   const [state, handleSubmit] = useForm("mzbnekbk");
+
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return <p>Thanks for joining!</p>;
   }
+
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Your Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        required
+      />
+      
+      <label htmlFor="email">Your Email</label>
       <input
         id="email"
-        type="email" 
+        type="email"
         name="email"
+        required
       />
       <ValidationError 
         prefix="Email" 
         field="email"
         errors={state.errors}
       />
+      
+      <label htmlFor="subject">Subject</label>
+      <input
+        id="subject"
+        type="text"
+        name="subject"
+        required
+      />
+      
+      <label htmlFor="message">Message</label>
       <textarea
         id="message"
         name="message"
+        rows="5"
+        required
       />
       <ValidationError 
         prefix="Message" 
         field="message"
         errors={state.errors}
       />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
+      
+      <div>Loading</div>
+      <div></div>
+      <div>Your message has been sent. Thank you!</div>
+      
+      <button type="submit" disabled={state.submitting}>Send Message</button>
     </form>
   );
 }
+
 function App() {
   return (
     <ContactForm />
   );
 }
+
 export default App;
