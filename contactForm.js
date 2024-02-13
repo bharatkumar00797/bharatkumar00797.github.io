4bfac502cd222152da23658e39877574d06900a8
bharatkumar00797 <154852574+bharatkumar00797@users.noreply.github.com>
2024-02-12 22:35:22 -0500

    Update contactForm.js

--- contactForm.js
+++ contactForm.js
@@ -1,67 +1,49 @@
+  /* paste this line in verbatim */
+  window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+  /* customize formbutton below*/     
+  formbutton("create", {
+    action: "https://formspree.io/f/mzbnekbk",
+    title: "How can we help?",
+    fields: [
+	    {
+        type: "text",
+        id: "name",
+        name: "name",
+	      required: true,
+        placeholder: " Your name",
+      },
+      { 
+        type: "email", 
+        id: "Email:", 
+        name: "email",
+        required: true,
+        placeholder: "your@email.com"
+      },
+	    {
+        type: "text",
+        id: "subject",
+        name: "subject",
+	      required: true,
+        placeholder: " Subject",
+     	 },
+        {
+        type: "textarea",
+        id: "Message:",
+        name: "message",
+	      required: true,
+        placeholder: "What's on your mind?",
+     	 },
+     
+	
+      { type: "submit" }      
+    ],
+    styles: {
+      title: {
+        backgroundColor: "gray"
+      },
+      button: {
+        backgroundColor: "gray"
+      }
+    }
+  });
 
-
-function ContactForm() {
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-  const [state, handleSubmit] = useForm("https://formspree.io/f/mzbnekbk");
-
-  if (state.succeeded) {
-    return <p>Thanks for joining!</p>;
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Your Name</label>
-      <input
-        id="name"
-        type="text"
-        name="name"
-        required
-      />
-      
-      <label htmlFor="email">Your Email</label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        required
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      
-      <label htmlFor="subject">Subject</label>
-      <input
-        id="subject"
-        type="text"
-        name="subject"
-        required
-      />
-      
-      <label htmlFor="message">Message</label>
-      <textarea
-        id="message"
-        name="message"
-        rows="5"
-        required
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      
-      <button type="submit" disabled={state.submitting}>Send Message</button>
-    </form>
-  );
-}
-
-function App() {
-  return (
-    <ContactForm />
-  );
-}
-
-export default App;
