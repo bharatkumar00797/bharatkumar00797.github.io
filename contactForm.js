daec3c07f7fcabd4168d9dd7bf3d0de7e89193a7
Bharatkumar Chandvani <154852574+bharatkumar00797@users.noreply.github.com>
2024-03-07 19:49:05 -0500

    Update contactForm.js

--- contactForm.js
+++ contactForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 function ContactForm()  {
 
@@ -64,6 +64,77 @@ function App() {
 }
 
 export default App;
+*/
+import React, { useEffect } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
+function ContactForm() {
+  const [state, handleSubmit] = useForm("https://formspree.io/f/mzbnekbk");
+
+  useEffect(() => {
+    if (state.succeeded) {
+      alert("Form submitted successfully!");
+    }
+  }, [state.succeeded]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Your Name</label>
+      <input
+        id="name"
+        type="text"
+        name="name"
+        required
+      />
+      
+      <label htmlFor="email">Your Email</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        required
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      
+      <label htmlFor="subject">Subject</label>
+      <input
+        id="subject"
+        type="text"
+        name="subject"
+        required
+      />
+      
+      <label htmlFor="message">Message</label>
+      <textarea
+        id="message"
+        name="message"
+        rows="5"
+        required
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      
+      <button type="submit" disabled={state.submitting}>Send Message</button>
+    </form>
+  );
+}
+
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+
+export default App;
+
+
  /*
 import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
